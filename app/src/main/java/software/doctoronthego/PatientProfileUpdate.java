beb796d311d01a13c0d312a46c3776781b68751a
architkaila <archit.kaila@gmail.com>
2017-11-19 15:36:56 +0530

    Added User Profile editing, using FireStore

--- app/src/main/java/software/doctoronthego/PatientProfileUpdate.java
+++ app/src/main/java/software/doctoronthego/PatientProfileUpdate.java
@@ -1,23 +1,74 @@
 package software.doctoronthego;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static software.doctoronthego.fragments.PatientSignIn.mAuth;
+
 public class PatientProfileUpdate extends AppCompatActivity {
+
+    String email;
+    EditText fname, lname, age, address;
+    ImageView image;
+    CollectionReference db = FirebaseFirestore.getInstance().collection("patientData");
+    FirebaseUser currentUser;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patient_profile_update);
 
-        EditText fname = findViewById(R.id.fname1);
-        EditText lname = findViewById(R.id.lname1);
-        EditText age = findViewById(R.id.age1);
-        EditText address = findViewById(R.id.address1);
-        ImageView image = findViewById(R.id.image_view);
+        currentUser = mAuth.getCurrentUser();
+        email = currentUser.getEmail();
+        fname = findViewById(R.id.fname1);
+        lname = findViewById(R.id.lname1);
+        age = findViewById(R.id.age1);
+        address = findViewById(R.id.address1);
+        //ImageView image = findViewById(R.id.image_view);
         Button update = findViewById(R.id.updateProfile);
+
+        update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Map<String, Object> docData = new HashMap<>();
+                docData.put("email", email);
+                docData.put("first_name", fname.getText().toString());
+                docData.put("last_name", lname.getText().toString());
+                docData.put("age", age.getText().toString());
+                docData.put("address", address.getText().toString());
+
+                db.document(email).set(docData).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.e("Patient ", "Data saved");
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.e("Error ", "Data Not Saved !");
+                    }
+                });
+
+                startActivity(new Intent(PatientProfileUpdate.this, PatientDetailsActivity.class));
+            }
+
+        });
     }
 }
--- app/src/main/java/software/doctoronthego/fragments/PatientProfile.java
+++ app/src/main/java/software/doctoronthego/fragments/PatientProfile.java
@@ -1,13 +1,27 @@
 package software.doctoronthego.fragments;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+
+import software.doctoronthego.PatientProfileUpdate;
 import software.doctoronthego.R;
+
+import static software.doctoronthego.fragments.PatientSignIn.mAuth;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -15,16 +29,62 @@ import software.doctoronthego.R;
 public class PatientProfile extends Fragment {
 
 
+    String userEmail;
+    FirebaseUser currentUser;
+    Button updateProfile;
+    DocumentReference db;
+    TextView email, fname, lname, age, address;
     public PatientProfile() {
         // Required empty public constructor
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        db.addSnapshotListener(getActivity(), new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(DocumentSnapshot documentSnapshot, FirebaseFirestoreException e) {
+                if (documentSnapshot.exists()) {
+                    String mFirstName = (String) documentSnapshot.get("first_name");
+                    String mLastName = (String) documentSnapshot.get("last_name");
+                    String mAge = (String) documentSnapshot.get("age");
+                    String mAddress = (String) documentSnapshot.get("address");
+                    fname.setText(mFirstName);
+                    lname.setText(mLastName);
+                    age.setText(mAge);
+                    email.setText(userEmail);
+                    address.setText(mAddress);
+                }
+            }
+        });
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_patient_profile, container, false);
+        View v = inflater.inflate(R.layout.fragment_patient_profile, container, false);
+        updateProfile = v.findViewById(R.id.updateProfile);
+        fname = v.findViewById(R.id.fname1);
+        lname = v.findViewById(R.id.lname1);
+        age = v.findViewById(R.id.age1);
+        address = v.findViewById(R.id.address1);
+        email = v.findViewById(R.id.email1);
+        currentUser = mAuth.getCurrentUser();
+        userEmail = currentUser.getEmail();
+
+        db = FirebaseFirestore.getInstance().collection("patientData").document(userEmail);
+        return v;
     }
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        updateProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getActivity(), PatientProfileUpdate.class));
+            }
+        });
+        super.onActivityCreated(savedInstanceState);
+    }
 }
