b03454eeec5f3f42269c5f85d9f10fa030c1013c
architkaila <archit.kaila@gmail.com>
2017-11-26 17:31:24 +0530

    Added functionality to add and update profile photo for patients using firebase storage.

--- app/src/main/java/software/doctoronthego/PatientProfileUpdate.java
+++ app/src/main/java/software/doctoronthego/PatientProfileUpdate.java
@@ -1,6 +1,8 @@
 package software.doctoronthego;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -9,12 +11,16 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -23,17 +29,22 @@ import static software.doctoronthego.fragments.PatientSignIn.mAuth;
 
 public class PatientProfileUpdate extends AppCompatActivity {
 
+    public static final int GALLERY_INTENT = 2;
     String email;
     EditText fname, lname, age, address;
     ImageView image;
+    Button addPhoto;
     CollectionReference db = FirebaseFirestore.getInstance().collection("patientData");
     FirebaseUser currentUser;
-
+    ProgressDialog mProgressDialogue;
+    StorageReference mStorage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patient_profile_update);
+
+        mStorage = FirebaseStorage.getInstance().getReference();
 
         currentUser = mAuth.getCurrentUser();
         email = currentUser.getEmail();
@@ -41,8 +52,23 @@ public class PatientProfileUpdate extends AppCompatActivity {
         lname = findViewById(R.id.lname1);
         age = findViewById(R.id.age1);
         address = findViewById(R.id.address1);
-        //ImageView image = findViewById(R.id.image_view);
+
         Button update = findViewById(R.id.updateProfile);
+
+        mProgressDialogue = new ProgressDialog(this);
+
+        image = findViewById(R.id.image_view);
+        addPhoto = findViewById(R.id.addPhoto);
+
+        addPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(intent, GALLERY_INTENT);
+            }
+        });
+
 
         update.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -70,5 +96,42 @@ public class PatientProfileUpdate extends AppCompatActivity {
             }
 
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == GALLERY_INTENT && resultCode == RESULT_OK) {
+
+            mProgressDialogue.setMessage("Uploading Image...");
+            mProgressDialogue.show();
+
+            final Uri uri = data.getData();
+
+            StorageReference filePath = mStorage.child("Photos").child(email);
+            filePath.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                    image.setImageURI(uri);
+                    mProgressDialogue.dismiss();
+                    Toast.makeText(PatientProfileUpdate.this, "Photo Upload Successful !", Toast.LENGTH_SHORT).show();
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(PatientProfileUpdate.this, "Phot upload Failed !", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+
     }
 }
--- app/src/main/java/software/doctoronthego/fragments/PatientProfile.java
+++ app/src/main/java/software/doctoronthego/fragments/PatientProfile.java
@@ -1,7 +1,9 @@
 package software.doctoronthego.fragments;
 
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -9,14 +11,19 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.squareup.picasso.Picasso;
 
 import software.doctoronthego.PatientProfileUpdate;
 import software.doctoronthego.R;
@@ -29,11 +36,17 @@ import static software.doctoronthego.fragments.PatientSignIn.mAuth;
 public class PatientProfile extends Fragment {
 
 
+    public static final int MAX_WIDTH = 500;
+    public static final int MAX_HEIGHT = 500;
     String userEmail;
     FirebaseUser currentUser;
     Button updateProfile;
     DocumentReference db;
     TextView email, fname, lname, age, address;
+    StorageReference mStorage;
+    ImageView patientProfile;
+    ProgressDialog mProgressDialogue;
+
     public PatientProfile() {
         // Required empty public constructor
     }
@@ -41,8 +54,10 @@ public class PatientProfile extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_patient_profile, container, false);
+
         updateProfile = v.findViewById(R.id.updateProfile);
         fname = v.findViewById(R.id.fname1);
         lname = v.findViewById(R.id.lname1);
@@ -51,14 +66,23 @@ public class PatientProfile extends Fragment {
         email = v.findViewById(R.id.email1);
         currentUser = mAuth.getCurrentUser();
         userEmail = currentUser.getEmail();
+        patientProfile = v.findViewById(R.id.patientPhoto);
+
+        mStorage = FirebaseStorage.getInstance().getReference();
+        mProgressDialogue = new ProgressDialog(getActivity());
 
         db = FirebaseFirestore.getInstance().collection("patientData").document(userEmail);
+
         return v;
     }
 
     @Override
     public void onStart() {
         super.onStart();
+
+        mProgressDialogue.setMessage("Loading...");
+        mProgressDialogue.show();
+
         db.addSnapshotListener(getActivity(), new EventListener<DocumentSnapshot>() {
             @Override
             public void onEvent(DocumentSnapshot documentSnapshot, FirebaseFirestoreException e) {
@@ -73,6 +97,24 @@ public class PatientProfile extends Fragment {
                     email.setText(userEmail);
                     address.setText(mAddress);
                 }
+            }
+        });
+
+        mStorage.child("Photos").child(userEmail).getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                //patientProfile.setImageURI(uri);
+
+                Picasso.with(getActivity()).load(uri).fit().centerCrop()
+                        .into(patientProfile);
+
+//                Picasso.with(getActivity())
+//                        .load(uri)
+//                        .resize(MAX_WIDTH,MAX_HEIGHT)
+//                        .onlyScaleDown()
+//                        .memoryPolicy(MemoryPolicy.NO_CACHE, MemoryPolicy.NO_STORE)
+//                        .into(patientProfile);
+                mProgressDialogue.dismiss();
             }
         });
     }
